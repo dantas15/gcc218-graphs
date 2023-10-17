2b1d6d63bbe4519d404ff2ef2ddf4654d480c1c0
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2023-10-17 13:00:04 -0300

    chore: backup

---
+++ exercises/03-beecrowd/01-st-petersburg-parties.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <functional>
+
+using namespace std;
+
+int main() {
+  int
+    peopleInCommunity, // n
+    friendShipEdges, // m
+    minFriendsToBeInvited; // k
+
+  cin >> peopleInCommunity >> friendShipEdges >> minFriendsToBeInvited;
+
+  // while (!cin.eof()) { /* In case of reading an input as a file */
+  while (cin) {
+    // considering the vertices start with 1 to n (total amount of people in the community)
+    vector<int>* adjList = new vector<int>[peopleInCommunity+1];
+    int* degree =  new int[peopleInCommunity+1];
+
+    // initializing the degree
+    for(int i = 1; i <= peopleInCommunity; i++) {
+      degree[i] = 0;
+    }
+
+    int u, v;
+
+    for (int i = 0; i < friendShipEdges; i++) {
+      cin >> u >> v; // reading all edges (u <-> v)
+
+      // avoiding repeated vertices and updating their degree (or valency)
+      if (find(adjList[u].begin(), adjList[u].end(), v) == adjList[u].end()) {
+        // in the case of undirected graphs
+        adjList[u].push_back(v); // u -> v
+        adjList[v].push_back(u); // v -> u
+
+        degree[u]++;
+        degree[v]++;
+      }
+    }
+
+    // PQ with everyone with less than K friends
+    priority_queue<pair<int, int>> PQ;
+    for (int i = 1; i <= peopleInCommunity; i++) {
+      if (degree[i] < minFriendsToBeInvited) {
+        PQ.push({ degree[i], i });
+      }
+    }
+
+    while (!PQ.empty()) {
+      // element <first, second>
+      int aux = PQ.top().second;
+      PQ.pop();
+
+      for (auto it = adjList[aux].begin(); it != adjList[aux].end(); it++) {
+        if (--degree[*it] == minFriendsToBeInvited-1) {
+          PQ.push({ degree[*it], *it });
+        }
+      }
+    }
+
+    // Output
+    bool first = true;
+    for (int i = 1; i <= peopleInCommunity; i++) {
+      if (degree[i] >= minFriendsToBeInvited) {
+        if (!first) {
+          cout << " ";
+        }
+        cout << i;
+        first = false;
+      }
+    }
+
+    if (first) {
+      cout << 0;
+    }
+
+    cout << endl;
+
+    cin
+      >> peopleInCommunity // n
+      >> friendShipEdges // m
+      >> minFriendsToBeInvited; // k
+  }
+  return 0;
+}
---
+++ exercises/03-beecrowd/notes.md
@@ -0,0 +1,27 @@
+# 3 - beecrowd exercises
+
+## St. Petersburg
+
+### Instructions
+
+- Each participant must have at least K quantity of friends in order to be invited.
+- Given the number of people in the community and their relationship listing:
+  - determine which of them should be called to the party
+  - this should be the ones chosen with the largest possible number of participants satisfying the constraint
+
+## Input
+
+- It ends with EOF (End of File)
+- The first line contains three integers N (1 ≤ N ≤ 1000), M and K (O ≤ K ≤ N):
+  1. Number of people in the community (N)
+  2. Number of friendships (M) (total amount of edges)
+  3. Minimum number of friends a person must have to be invited (K)
+
+## Output
+
+## How to solve it
+
+- <https://colab.research.google.com/drive/1zOVMPKzUKm4J0JWp9TzdddQwdQdGbjmR#scrollTo=IP7t3m7ehCdu>
+- <https://www.ime.usp.br/~pf/algoritmos/aulas/lista.html>
+
+*/
